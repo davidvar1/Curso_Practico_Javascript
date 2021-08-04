04d1bc55d7be45c9563d62f7fe6d6f456edef210
davidvar1 <davidvar1@outlook.com>
2021-08-04 17:31:43 -0500

    Nuevos archivos

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Curso_Practico_Javascript
+
+## Taller Figuras Geometricas
+
+- Primer Paso: Definir las formulas
+- Segundo Paso: Implementar las formulas en javascript
+- Tercer Paso: Crear Funciones
+- Cuarto Paso: Integrar JS a HTML
+
---
+++ figuras.js
@@ -0,0 +1,8 @@
+const ladoCuadrado = 5;
+console.log ("los lados del cuadrado miden " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log ("El perimetro cuadrado es " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log ("El area de cuadrado es " + areaCuadrado + "cm");
